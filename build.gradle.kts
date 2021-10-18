0aefad2d7aee1acf8b377f00bc25bf0247bdff37
stefan@bettercoding.dev <stefan@bettercoding.dev>
2021-10-18 08:15:17 +0200

    complete implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.konan.properties.loadProperties
 
 plugins {
     kotlin("jvm") version "1.5.31"
     application
+    id("com.jetbrains.exposed.gradle.plugin") version "0.2.1"
 }
 
 group = "dev.bettercoding"
@@ -12,12 +14,35 @@ repositories {
     mavenCentral()
 }
 
-
-
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
 }
 
 application {
     mainClass.set("MainKt")
+}
+
+dependencies {
+    implementation("org.flywaydb:flyway-core:8.0.1")
+    implementation("org.postgresql:postgresql:42.2.24")
+    implementation("com.zaxxer:HikariCP:5.0.0")
+    implementation("org.jetbrains.exposed:exposed-core:0.35.2")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.35.2")
+}
+
+exposedCodeGeneratorConfig {
+    val dbProperties = loadProperties("${projectDir}/db.properties")
+    configFilename = "exposedConf.yml"
+    user = dbProperties["dataSource.user"].toString()
+    password = dbProperties["dataSource.password"].toString()
+    databaseName = dbProperties["dataSource.database"].toString()
+    databaseDriver = dbProperties["dataSource.driver"].toString()
+}
+
+sourceSets.main {
+    java.srcDirs("build/tables")
+}
+
+tasks.generateExposedCode {
+    dependsOn("clean")
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,36 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import dev.bettercoding.generated.Users
+import org.flywaydb.core.Flyway
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun initDatabase(){
+    val hikariConfig = HikariConfig("db.properties")
+    val dataSource = HikariDataSource(hikariConfig)
+
+    val flyway = Flyway.configure().dataSource(dataSource).load()
+    flyway.migrate()
+
+    Database.connect(dataSource)
+}
+
+fun main() {
+    initDatabase()
+
+    transaction {
+        Users.deleteAll()
+
+        Users.insert {
+            it[name] = "Stefan"
+            it[age] = 30
+        }
+
+        Users.selectAll().forEach {
+            println(it[Users.name])
+        }
+    }
 }
